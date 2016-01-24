83c7a12d7be6e3f8f7c088d433378a912370594b
unknown <ncparikh22@gmail.com>
2016-01-24 14:38:15 -0500

    changes for pushing change to remote repo

--- first_dir/third.php
+++ first_dir/third.php
@@ -1,3 +1,4 @@
 My Third file that I have added in this project..
 last file in project
 <?php echo "Hello Miss Third..."; ?>
+more info for remote repo. tried file
--- secondary.php
+++ secondary.php
@@ -1,2 +1,3 @@
 My Second file that I have added in this project..
 <?php echo "Hello Miss second..."; ?>
+more info for remote repo.
